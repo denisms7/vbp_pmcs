2096a5e85417faea5a479d4f51a27aaec10fb6f5
Denis Muniz Silva <denis.m.s.777@hotmail.com>
2022-07-12 03:25:09 -0300

    Create base_vbp.csv

--- app.py
+++ app.py
@@ -4,27 +4,25 @@ from dash.dependencies import Input, Output
 
 
 import pandas as pd
+import numpy as np
 import openpyxl
 import plotly.express as px
 import plotly.graph_objects as go
 
-df = pd.DataFrame(pd.read_excel('base_vbp.xlsx'))
 
-df_cidades = df['Município'].value_counts().index
-df_produto = df['Cultura'].value_counts().index
 
 
 app = dash.Dash(__name__)
 server = app.server
 
-
+df = pd.DataFrame(pd.read_excel('base_vbp.xlsx'))
 
 
 # =========  Layout  =========== #
 app.layout = html.Div(children=[
     html.Div([
-        dcc.Dropdown(df_cidades,'Centenário do Sul', multi=True, id='id_cidade'),
-        dcc.Dropdown(df_produto, multi=True, id='id_produto')
+        dcc.Dropdown(df['Município'].value_counts().index, multi=True, id='id_cidade'),
+        dcc.Dropdown(df['Cultura'].value_counts().index, multi=True, id='id_produto')
     ], className=''),
 
     html.Div([dcc.Graph(id='fig_vbp_geral')], className=''),
@@ -43,12 +41,13 @@ app.layout = html.Div(children=[
     ])
 
 def renderizar_graficos(id_cidade, id_produto):
-    df_filtro_cidade = df[df['Município'].isin([id_cidade])]
-    graf_linha = df_filtro_cidade.groupby(by=['Município', 'Safra'])['VBP'].sum().to_frame().reset_index()
+    df_filtro_cidade = df[df['Município'].isin(pd.Series(id_cidade))]
 
-    print(df_filtro_cidade)
+    graf_linha = df_filtro_cidade.groupby(by=['Município', 'Safra'])['VBP'].apply(np.sum).to_frame().reset_index()
 
-    fig_vbp_geral = px.line(graf_linha, x="Safra", y="VBP", color='Município', text="VBP")
+    fig_vbp_geral = px.line(graf_linha, x='Safra', y='VBP', color='Município', text='VBP')
+
+
     fig_produto = px.bar(df_filtro_cidade, x='Safra', y='Produção')
 
     fig_vbp_geral.update_layout(template='plotly_dark', height=500, transition={"duration": 400})
