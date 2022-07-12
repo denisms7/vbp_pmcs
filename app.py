db3d1b0da4cddab552ae3951a2cd193c8ef6317c
Denis Muniz Silva <denis.m.s.777@hotmail.com>
2022-07-12 02:42:56 -0300

    Create base_vbp.csv

--- app.py
+++ app.py
@@ -4,52 +4,57 @@ from dash.dependencies import Input, Output
 
 
 import pandas as pd
+import openpyxl
 import plotly.express as px
 import plotly.graph_objects as go
 
+df = pd.DataFrame(pd.read_excel('base_vbp.xlsx'))
 
+df_cidades = df['Município'].value_counts().index
+df_produto = df['Cultura'].value_counts().index
 
 
 app = dash.Dash(__name__)
 server = app.server
 
 
-df = pd.DataFrame(pd.read_excel('base_vbp.xlsx'))
-
-df_cultura = df['Cultura'].value_counts().to_frame().reset_index().drop('Cultura', axis=1)
-df_cidades = df['Município'].value_counts().to_frame().reset_index().drop('Município', axis=1)
-
 
 
 # =========  Layout  =========== #
 app.layout = html.Div(children=[
+    html.Div([
+        dcc.Dropdown(df_cidades,'Centenário do Sul', multi=True, id='id_cidade'),
+        dcc.Dropdown(df_produto, multi=True, id='id_produto')
+    ], className=''),
 
-    dcc.Dropdown(
-        ['New York City', 'Montreal', 'San Francisco'],
-        ['Montreal', 'San Francisco'],
-        multi=True, id='id_cidade'
-),
-
-        html.Div([html.Div([dcc.Graph(id='fig_vbp_geral')], className='')]),
-
+    html.Div([dcc.Graph(id='fig_vbp_geral')], className=''),
+    html.Div([dcc.Graph(id='fig_produto')], className=''),
 
 
 ])
 # =========  Callback  =========== #
 @app.callback([
         Output('fig_vbp_geral', 'figure'),
+        Output('fig_produto', 'figure'),
     ],
     [
         Input('id_cidade', 'value'),
+        Input('id_produto', 'value'),
     ])
 
-def renderizar_graficos(id_cidade):
+def renderizar_graficos(id_cidade, id_produto):
+    df_filtro_cidade = df[df['Município'].isin([id_cidade])]
+    graf_linha = df_filtro_cidade.groupby(by=['Município', 'Safra'])['VBP'].sum().to_frame().reset_index()
 
-    fig_vbp_geral = px.bar(df_cidades, x='City', y=check_agrupador, color='City')
-    fig_vbp_geral.update_layout(template='plotly_dark', height=250, transition={"duration": 400})
+    print(df_filtro_cidade)
 
+    fig_vbp_geral = px.line(graf_linha, x="Safra", y="VBP", color='Município', text="VBP")
+    fig_produto = px.bar(df_filtro_cidade, x='Safra', y='Produção')
 
-    return fig_vbp_geral
+    fig_vbp_geral.update_layout(template='plotly_dark', height=500, transition={"duration": 400})
+    fig_produto.update_layout(template='plotly_dark', height=250, transition={"duration": 400})
+
+    return fig_vbp_geral, fig_produto
 
 if __name__ == "__main__":
     app.run_server(host='0.0.0.0', port=80, debug=True)
