26f1d1b70f73ebf667fec5ba0cbc2841f16997ea
Denis Muniz Silva <denis.m.s.777@hotmail.com>
2022-07-18 11:24:35 -0300

    att lay

--- app.py
+++ app.py
@@ -23,91 +23,75 @@ df['Área (ha)'] = df['Área (ha)'].apply(lambda x: str(x).replace(' ','').repla
 
 # =========  Layout  =========== #
 app.layout = html.Div(children=[
-html.Title('VBP Municipal'),
-html.Div([
-    html.H2('Comparativo entre Cidades'),
-    html.Div([
+    html.Div([ # linha 1
+        html.H2('Comparativo entre Cidades'),
         html.Div([
             dcc.Dropdown(df['Município'].value_counts().index.sort_values(ascending=True),'Centenário do Sul', className='form-control', placeholder='Selecione a(s) Cidades', multi=True, id='id_cidade'),
         ], className='col-md-6 p-1'),
         html.Div([
             dcc.RadioItems(['Área (ha)',
-       'Rebanho Estático', 'Abate / Comercialização', 'Peso', 'Produção',
-       'VBP'],'Área (ha)',  id='check_rank', className='m-1', inputStyle={'margin-right': '5px', 'margin-left': '5px'}),
+            'Rebanho Estático', 'Abate / Comercialização', 'Peso', 'Produção',
+            'VBP'],'Área (ha)', id='check_rank', className='m-1', inputStyle={'margin-right': '5px', 'margin-left': '5px'}),
         ], className='col-md-6 p-1'),
+    ], className='row'), # fim linha 1
 
-    ], className='row'),
-    html.Div([
-        html.Div([
-            html.Div([dcc.Graph(id='fig_vbp_geral')], className=''),
-        ], className='col-md-6 p-1'),
+    html.Div([ # linha 2
+        html.Div([html.Div([dcc.Graph(id='fig_vbp_geral')], className=''),], className='col-md-6 p-1'),
+        html.Div([html.Div([dcc.Graph(id='fig_rank')], className=''),], className='col-md-6 p-1'),
+    ], className='row'), # fim linha 2
 
-        html.Div([
-            html.Div([dcc.Graph(id='fig_rank')], className=''),
-        ], className='col-md-6 p-1'),
-
-    ], className='row'),
-html.Div([
+    html.Div([ # linha 3
         html.Div([
             dcc.Dropdown(df['Cultura'].value_counts().index.sort_values(ascending=True),className='form-control', placeholder='Selecione a Cultura',  id='id_produto'),
         ], className='col-md-6 p-1'),
-
         html.Div([
+            ], className='col-md-6 p-1'),
+    ], className='row'), # fim linha 3
 
+    html.Div([ # linha 4
+        html.Div([dcc.RadioItems(['Área (ha)',
+           'Rebanho Estático', 'Abate / Comercialização', 'Peso', 'Produção',
+           'VBP'],'Área (ha)',  id='check_producao_0', className='m-1', inputStyle={'margin-right': '5px', 'margin-left': '5px'}),
         ], className='col-md-6 p-1'),
-
-        html.Div([
-            html.Div([dcc.Graph(id='fig_produto')], className=''),
+        html.Div([dcc.RadioItems(['Área (ha)',
+           'Rebanho Estático', 'Abate / Comercialização', 'Peso', 'Produção',
+           'VBP'],'Área (ha)',  id='check_producao_1', className='m-1', inputStyle={'margin-right': '5px', 'margin-left': '5px'}),
         ], className='col-md-6 p-1'),
+    ], className='row'), # fim linha 4
 
-        html.Div([
-            html.Div([dcc.Graph(id='fig_area')], className=''),
-        ], className='col-md-6 p-1'),
-
+    html.Div([ # linha 5
+        html.Div([html.Div([dcc.Graph(id='fig_an_1')], className=''),], className='col-md-6 p-1'),
+        html.Div([html.Div([dcc.Graph(id='fig_an_0')], className=''),], className='col-md-6 p-1'),
     ], className='row'),
 
+    html.Div([ # linha 6
+        html.Div([dcc.RadioItems(['Media', 'Mediana'],'Media' ,  id='check_media', className='m-1', inputStyle={'margin-right': '5px', 'margin-left': '5px'}),], className='col-md-4 p-1'),
+        html.Div([], className='col-md-4 p-1'),
+        html.Div([dcc.RadioItems(['Minimo', 'Maximo'],'Minimo',  id='check_min_max', className='m-1', inputStyle={'margin-right': '5px', 'margin-left': '5px'}),], className='col-md-4 p-1'),
+    ], className='row'), # fim da linha 6
 
-    html.H2('Análise Estadual'),
-    html.Div([
-        html.Div([
-            dcc.RadioItems(['Media', 'Mediana'],'Media' ,  id='check_media', className='m-1', inputStyle={'margin-right': '5px', 'margin-left': '5px'}),
-        ], className='col-md-4 p-1'),
-        html.Div([
+    html.Div([ # linha 7
+        html.Div([html.Div([dcc.Graph(id='fig_media')], className=''),], className='col-md-4 p-1'),
+        html.Div([html.Div([dcc.Graph(id='fig_min_max')], className=''),], className='col-md-4 p-1'),
+        html.Div([html.Div([dcc.Graph(id='fig_total')], className=''),], className='col-md-4 p-1'),
+    ], className='row'), # fim da linha 7
 
-        ], className='col-md-4 p-1'),
-        html.Div([
-            dcc.RadioItems(['Minimo', 'Maximo'],'Minimo' ,  id='check_min_max', className='m-1', inputStyle={'margin-right': '5px', 'margin-left': '5px'}),
-        ], className='col-md-4 p-1'),
-
-    ], className='row'),
-    html.Div([
-        html.Div([
-            html.Div([dcc.Graph(id='fig_media')], className=''),
-        ], className='col-md-4 p-1'),
-
-        html.Div([
-            html.Div([dcc.Graph(id='fig_total')], className=''),
-        ], className='col-md-4 p-1'),
-
-            html.Div([
-            html.Div([dcc.Graph(id='fig_min_max')], className=''),
-        ], className='col-md-4 p-1'),
-
+    html.Div([ # linha 6
+        html.Div([html.P(['Desenvolvido por Denis muniz Silva']),], className='col-md-12 p-1'),
     ], className='row'),
 
-
-
-], className='row p-2'),
 ], className='container-fluid')
+
 # =========  Callback  =========== #
 @app.callback([
         Output('fig_vbp_geral', 'figure'),
-        Output('fig_produto', 'figure'),
-        Output('fig_media', 'figure'),
-        Output('fig_min_max', 'figure'),
-        Output('fig_total', 'figure'),
-        Output('fig_area', 'figure'),
         Output('fig_rank', 'figure'),
+        Output('fig_an_0', 'figure'),
+        Output('fig_an_1', 'figure'),
+        Output('fig_media', 'figure'),
+        Output('fig_total', 'figure'),
+        Output('fig_min_max', 'figure'),
+
     ],
     [
         Input('id_cidade', 'value'),
@@ -115,9 +99,11 @@ html.Div([
         Input('check_media', 'value'),
         Input('check_min_max', 'value'),
         Input('check_rank', 'value'),
+        Input('check_producao_1', 'value'),
+        Input('check_producao_0', 'value'),
     ])
 
-def renderizar_graficos(id_cidade, id_produto, check_media, check_min_max, check_rank):
+def renderizar_graficos(id_cidade, id_produto, check_media, check_min_max, check_rank,check_producao_0, check_producao_1):
     df_filtro_cidade = df[df['Município'].isin(pd.Series(id_cidade))]
 
     df_media = df.groupby(by=['Safra'])['VBP'].mean().reset_index()
@@ -137,8 +123,6 @@ def renderizar_graficos(id_cidade, id_produto, check_media, check_min_max, check
 
     df_statistico = pd.concat([df_media, df_mediana, df_maximo,df_minimo, df_total], axis=1).reset_index()
 
-
-
     v_lista = []
     for x in df_filtro_cidade['Município'].unique().tolist():
         for n in df['Safra'].unique().tolist():
@@ -147,17 +131,14 @@ def renderizar_graficos(id_cidade, id_produto, check_media, check_min_max, check
     df_rank5 = pd.concat(v_lista)
 
     fig_rank = px.scatter(df_rank5, x='Safra', y=check_rank, size=check_rank , color='Município', hover_data=['Cultura', check_rank])
-    fig_rank.update_layout(template='plotly_dark', transition={"duration": 400}, title=f"RANK 5 {check_rank.upper()}")
-
 
 
     graf_linha = df_filtro_cidade.groupby(by=['Município', 'Safra'])['VBP'].apply(np.sum).to_frame().reset_index()
     fig_vbp_geral = px.line(graf_linha, x='Safra', y='VBP', color='Município')
 
     graf_produto = df_filtro_cidade[df_filtro_cidade['Cultura'].isin(pd.Series(id_produto))]
-    fig_produto = px.bar(graf_produto, x='Safra', y='Produção', color='Município', text='Produção')
-
-    fig_area = px.bar(graf_produto, x='Safra', y='Área (ha)', color='Município', text='Área (ha)')
+    fig_an_0 = px.bar(graf_produto, x='Safra', y=check_producao_0, color='Município', text=check_producao_0)
+    fig_an_1 = px.bar(graf_produto, x='Safra', y=check_producao_1, color='Município', text=check_producao_1)
 
     fig_media = go.Figure()
     if check_media == 'Media':
@@ -180,12 +161,15 @@ def renderizar_graficos(id_cidade, id_produto, check_media, check_min_max, check
 
 
     fig_vbp_geral.update_layout(template='plotly_dark', transition={"duration": 400}, title="VBP Bruto Anual")
-    fig_produto.update_layout(template='plotly_dark', transition={"duration": 400}, barmode='group', title=f"PRODUÇÃO {id_produto} (exeto animais)")
-    fig_area.update_layout(template='plotly_dark', transition={"duration": 400}, barmode='group', title="COMPARAÇÃO POR AREA")
-    fig_total.update_layout(template='plotly_dark',height=300 , transition={"duration": 400}, title="TOTAL POR SAFRA - VBP R$")
-    fig_rank.update_layout(template='plotly_dark', transition={"duration": 400}, barmode='group')
+    fig_rank.update_layout(template='plotly_dark', transition={"duration": 400}, title=f"RANK 5 {check_rank.upper()}")
 
-    return fig_vbp_geral, fig_produto, fig_media, fig_min_max, fig_total, fig_area, fig_rank
+    fig_an_0.update_layout(template='plotly_dark', transition={"duration": 400}, barmode='group', title=f"COMPARAÇÃO {check_producao_0.upper()}")
+    fig_an_1.update_layout(template='plotly_dark', transition={"duration": 400}, barmode='group', title=f"COMPARAÇÃO {check_producao_1.upper()}")
+
+    fig_total.update_layout(template='plotly_dark',height=300 , transition={"duration": 400}, title="TOTAL POR SAFRA - VBP R$")
+
+
+    return fig_vbp_geral, fig_rank, fig_an_0, fig_an_1, fig_media, fig_min_max, fig_total
 
 if __name__ == "__main__":
     app.run_server(host='0.0.0.0', port=80, debug=True)
